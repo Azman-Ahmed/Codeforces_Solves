452ee4ddda6534b82eecda4ded7bcd3b5702fafc
Azman Ahmed <ahmed.azman.bd0007@gmail.com>
2024-05-26 20:30:39 +0600

    Codeforces Round 948 (Div. 2)

---
+++ Codeforces Round 948 (Div. 2)/A.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define lli long long int
+#define forn(n) for(int i = 0; i<n; i++)
+#define pr(n) cout<<n<<endl;
+#define all(x) (x).begin(), (x).end()
+#define no cout<<"NO"<<endl
+#define yes cout<<"YES"<<endl
+
+using namespace std;
+
+
+lli dp[3][1000005], trace[3][1000005];
+
+const int MOD = 1e9 + 7;
+
+void solve()
+{
+    lli n;
+    cin>>n;
+    vector<lli> veca(n);
+    for(int i = 0; i<n; i++) cin>>veca[i];
+
+
+
+
+}
+
+int main()
+{
+
+	int t;
+	cin>>t;
+
+	lli cse = 1;
+	while(t--)
+    {
+//        cout<<"Case "<<cse++<<": ";
+        solve();
+
+    }
+
+}
