c3fe950018294c0b7f358842a3ffd63fd08448cb
Azman Ahmed <ahmed.azman.bd0007@gmail.com>
2024-06-27 22:03:21 +0600

    Educational Codeforces Round 167

---
+++ Educational Codeforces Round 167/A.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define lli long long int
+#define forn(n) for(int i = 0; i<n; i++)
+#define pr(n) cout<<n<<endl;
+#define all(x) (x).begin(), (x).end()
+#define no cout<<"NO"<<endl
+#define yes cout<<"YES"<<endl
+
+using namespace std;
+
+
+lli dp[3][1000005], trace[3][1000005];
+
+const int MOD = 1e9 + 7;
+
+
+void solve()
+{
+    int x, y;
+    cin>>x>>y;
+
+    if(y>=-1) cout<<"YES"<<endl;
+    else if(y<-1) cout<<"NO"<<endl;
+
+
+
+}
+
+int main()
+{
+
+	int t;
+	cin>>t;
+
+	lli cse = 1;
+	while(t--)
+    {
+//        cout<<"Case "<<cse++<<": ";
+        solve();
+
+    }
+
+}
